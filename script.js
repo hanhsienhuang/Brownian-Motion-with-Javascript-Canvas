cc3dfd9f191b0a34b79adbde134efd04fe710984
Han-Hsien Huang <hu8108@gmail.com>
2020-04-03 23:13:00 -0500

    Add plotting of speed distribution

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 var canvas = document.getElementById('brownian-motion');
+var canvas2 = document.getElementById('speed-plot');
 var width = canvas.width;
 var height = canvas.height;
 var size = 10;
@@ -6,10 +7,13 @@ var alpha = 0.25;
 var lambda = 0.01;
 var particles = [];
 var stop = false;
+var color = "direction"
+var refresh_interval = 20;
 
 var Particle = function(x,y){
     this.x = [x,y];
     this.v = [0,0];
+    this.speed = 0;
     this.orient = 0;
     this.update = function(){
         this.x[0] = (this.x[0] + this.v[0] + width) % width;
@@ -21,7 +25,22 @@ var Particle = function(x,y){
                 this.orient += Math.PI;
             }
         }
+        this.speed = Math.sqrt(this.v[0]**2 + this.v[1]**2);
     }
+}
+
+var set_num_particle = function(N){
+    var n = particles.length;
+    if(n < N){
+        for(let i=0; i<N-n; i++){
+            particles.push(new Particle(Math.random()*width, Math.random()*height));
+        }
+    }else{
+        for(let i=0; i<n-N; i++){
+            particles.pop();
+        }
+    }
+
 }
 
 var toggle_stop = function(){
@@ -107,22 +126,30 @@ var update_num = function(){
     text.value = particles.length;
 }
 
-var set_num_particle = function(N){
-    var n = particles.length;
-    if(n < N){
-        for(let i=0; i<N-n; i++){
-            particles.push(new Particle(Math.random()*width, Math.random()*height));
+var color_input = function(){
+    var elements = document.getElementsByName('color-control');
+    for(e of elements){
+        if(e.checked){
+            color = e.value;
+            break;
         }
-    }else{
-        for(let i=0; i<n-N; i++){
-            particles.pop();
+    }
+}
+
+var update_color = function(){
+    var elements = document.getElementsByName('color-control');
+    for(e of elements){
+        if(e.value == color){
+            e.checked = true;
+            break;
         }
     }
 
 }
 
 if (canvas.getContext) {
-    var ctx = canvas.getContext('2d');
+    var ctx  = canvas.getContext('2d');
+    var ctx2 = canvas2.getContext('2d');
     var draw_particle_orig = function(){
         ctx.beginPath();
         var half_length = size/2;
@@ -147,13 +174,87 @@ if (canvas.getContext) {
         ctx.setTransform(1, 0, 0, 1, 0, 0);
     }
 
-    var update = function(){
-        //ctx.clearRect(0, 0, canvas.width, canvas.height);
+    var draw_particles = function(){
         ctx.save()
         ctx.fillStyle = 'rgba(255, 255, 255, 0.1)';
         ctx.fillRect(0, 0, width, height);
         ctx.restore()
 
+        switch(color){
+            case "speed":
+                var sigma = alpha/Math.sqrt(1-(1-Math.max(lambda, 0.01))**2);
+                var speed99 = 3*sigma;
+                var color_fn = function(p){
+                    var intensity = Math.min( p.speed/speed99, 1)*255;
+                    return `rgb(${intensity}, 0, ${255-intensity})`;
+                }
+                break;
+            case "direction":
+                var color_fn = function(p){
+                    return `hsl(${p.orient/2/Math.PI*360}, 100%, 50%)`;
+                }
+                break;
+            default:
+                var color_fn = function(p){
+                    return "color";
+                }
+        }
+        particles.forEach(function(p){
+            draw_particle(p, color_fn(p));
+        });
+    }
+
+    var plot_speed = function(){
+        ctx2.clearRect(0, 0, width, height);
+        var speeds = [];
+        for(p of particles){
+            speeds.push(p.speed);
+        }
+        speeds.sort(function(a,b){return a-b});
+        console.log(speeds);
+        var left = 50, right = width-20, top = 20, bot = height-50;
+        var plot_h = top-bot, plot_w = right-left;
+        ctx2.strokeRect(left, top, right-left, bot-top);
+        ctx2.font = "20px serif";
+        ctx2.textAlign = "right";
+        ctx2.fillText("1", left-10, top);
+        ctx2.textBaseline = "top";
+        ctx2.fillText("0", left-10, bot+10);
+        ctx2.fillText("speed", right, bot+10);
+
+        if(alpha!=0 && lambda != 0){
+            ctx2.save()
+            ctx2.strokeStyle = "blue";
+            ctx2.beginPath();
+            ctx2.moveTo(left, bot);
+            var num = 20;
+            for(let i=1; i<= num; i++){
+                var x = i/num;
+                var y = 1-Math.exp(-9/2*x**2);
+                ctx2.lineTo(left+x*plot_w, bot+y*plot_h);
+            }
+            ctx2.stroke();
+            ctx2.restore();
+        }
+        var sigma = alpha/Math.sqrt(1-(1-Math.max(lambda, 0.01))**2);
+        var speed99 = 3*sigma;
+        var x = 0, y = 0;
+        ctx2.beginPath();
+        ctx2.moveTo(left, bot);
+        var num = particles.length;
+        for(i in speeds){
+            s = speeds[i];
+            var newx = Math.min(s/speed99, 1), newy = (parseInt(i)+1)/num;
+            ctx2.lineTo(left+newx*plot_w, bot+y*plot_h);
+            ctx2.lineTo(left+newx*plot_w, bot+newy*plot_h);
+            x = newx;
+            y = newy;
+            if(x >= 1) break;
+        }
+        ctx2.stroke();
+    }
+
+    var update = function(){
         particles.forEach(function(p){
             p.v[0] *= (1-lambda);
             p.v[1] *= (1-lambda);
@@ -161,10 +262,11 @@ if (canvas.getContext) {
             p.v[0] += noise[0]*alpha;
             p.v[1] += noise[1]*alpha;
             p.update();
-            draw_particle(p, `hsl(${p.orient/2/Math.PI*360}, 100%, 50%)`);
         });
+        draw_particles();
+        plot_speed();
         if(!stop){
-            setTimeout(function(){update()}, 20);
+            setTimeout(function(){update()}, refresh_interval);
         }
     }
 
@@ -172,6 +274,7 @@ if (canvas.getContext) {
     update_alpha();
     update_lambda();
     update_num();
+    update_color();
     update();
 } else {
     // canvas-unsupported code here
